3a1e1803108a982ac2e60858a59d2aeb3ccb8187
Adam Cooper <amcooper@gmail.com>
2014-12-14 22:41:21 -0500

    Links almost work

--- serialization_client_browser.js
+++ serialization_client_browser.js
@@ -25,7 +25,7 @@ var hashPackage = function(input) {
 	if (userNameElement.value !== "") { userName = userNameElement.value; }
 
 	// Magic words
-	//if (displayText === "(table flip)") { displayText = "(╯°□°）╯︵ ┻━┻"; }
+	if (displayText === "(table flip)") { displayText = "(╯°□°）╯︵ ┻━┻"; }
 	if (displayText.substr(0,5) === "/yell") {
 		if (displayText === "/yell") {
 			displayText = "AAARRGHH!";
@@ -34,10 +34,16 @@ var hashPackage = function(input) {
 		}
 	}
 
+	//Special parsings
 	if ((displayText.indexOf("http://") > -1) || (displayText.indexOf("https://") > -1)) {
 		//Convert URL to <a href="URL">URL</a>
-		var interText01 ;
-		urlText = ;
+		var interText01 = displayText.substr(displayText.indexOf("http")); 
+		console.log("interText01: " + interText01);
+		var interText02 = interText01.substr(0,interText01.indexOf(" "));
+		console.log("interText02: " + interText02);
+		var interText03 = 
+		//Check for images here.
+		displayText = "<a href=\"" + interText02 + "\">" + interText02 + "</a> " + ;
 	}
 
 	var hash = {
